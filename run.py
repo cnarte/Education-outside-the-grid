55c816d717ed2f37e425555d0791c2078819ab81
cnarte <56212417+cnarte@users.noreply.github.com>
2021-07-10 04:28:11 +0530

    Update run.py

--- run.py
+++ run.py
@@ -19,24 +19,24 @@ def create_app():
     # Initialize our ngrok settings into Flask
     app.config.from_mapping(
         BASE_URL="http://localhost:5000",
-        USE_NGROK=os.environ.get("USE_NGROK", "False") == "True" and os.environ.get("WERKZEUG_RUN_MAIN") != "true"
+        USE_NGROK = "True"
     )
 
-    if app.config.get("ENV") == "development" and app.config["USE_NGROK"]:
+#     if app.config.get("ENV") == "development" and app.config["USE_NGROK"]:
         # pyngrok will only be installed, and should only ever be initialized, in a dev environment
-        from pyngrok import ngrok
+    from pyngrok import ngrok
 
-        # Get the dev server port (defaults to 5000 for Flask, can be overridden with `--port`
-        # when starting the server
-        port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 5000
+    # Get the dev server port (defaults to 5000 for Flask, can be overridden with `--port`
+    # when starting the server
+    port = 5000
 
-        # Open a ngrok tunnel to the dev server
-        public_url = ngrok.connect(port).public_url
-        print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+    # Open a ngrok tunnel to the dev server
+    public_url = ngrok.connect(port).public_url
+    print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
 
-        # Update any base URLs or webhooks to use the public ngrok URL
-        app.config["BASE_URL"] = public_url
-        init_webhooks(public_url)
+    # Update any base URLs or webhooks to use the public ngrok URL
+    app.config["BASE_URL"] = public_url
+    init_webhooks(public_url)
 
     # ... Initialize Blueprints and the rest of our app
 
