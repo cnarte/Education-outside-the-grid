677255aa61f4049bc5473cbac33a1c8bc1d43bdd
cnarte <56212417+cnarte@users.noreply.github.com>
2021-07-10 04:21:08 +0530

    Update run.py

--- run.py
+++ run.py
@@ -12,7 +12,35 @@ from flask import Flask, render_template, request
 # from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 from flask import send_from_directory
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+
+    # Initialize our ngrok settings into Flask
+    app.config.from_mapping(
+        BASE_URL="http://localhost:5000",
+        USE_NGROK=os.environ.get("USE_NGROK", "False") == "True" and os.environ.get("WERKZEUG_RUN_MAIN") != "true"
+    )
+
+    if app.config.get("ENV") == "development" and app.config["USE_NGROK"]:
+        # pyngrok will only be installed, and should only ever be initialized, in a dev environment
+        from pyngrok import ngrok
+
+        # Get the dev server port (defaults to 5000 for Flask, can be overridden with `--port`
+        # when starting the server
+        port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 5000
+
+        # Open a ngrok tunnel to the dev server
+        public_url = ngrok.connect(port).public_url
+        print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+
+        # Update any base URLs or webhooks to use the public ngrok URL
+        app.config["BASE_URL"] = public_url
+        init_webhooks(public_url)
+
+    # ... Initialize Blueprints and the rest of our app
+
+    return app
+app = create_app()
 
 path = ""
 PORT = int(os.environ.get("PORT", 8000))
@@ -91,6 +119,7 @@ def send_video():
 
 if __name__=='__main__':
     app.run(port= 5000, debug=False )
+    
     # http_server = WSGIServer(("", PORT), app)
     # http_server.serve_forever()
 
